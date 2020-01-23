1fa512e545e8eb0f677e3817e9f66d59e4b64452
Chris Pritchard <cpritchard@Chriss-MacBook-Pro.local>
2020-01-24 11:54:22 +1300

    gathering of tasks

--- main.go
+++ main.go
@@ -14,7 +14,10 @@ func getDataForURL(url string, apiKey string) map[string]interface{} {
 	req.Header.Add("Authorization", bearer)
 
 	client := &http.Client{}
-	resp, _ := client.Do(req)
+	resp, err := client.Do(req)
+	if err != nil {
+		os.Exit(2)
+	}
 	body, _ := ioutil.ReadAll(resp.Body)
 
 	var f interface{}
@@ -48,44 +51,42 @@ func getContactsMap(json map[string]interface{}) map[string]string {
 }
 
 type timelog struct {
-	taskID, userID, trackedDate string
-	hours                       float64
+	user, trackedDate string
+	hours             float64
 }
 
-func getTimelogs(json map[string]interface{}) []timelog {
+func getTimelogMap(json map[string]interface{}, contacts map[string]string) map[string][]timelog {
 	var data = json["data"].([]interface{})
 
-	var result = make([]timelog, len(data))
-	for i, k := range data {
+	var result = make(map[string][]timelog)
+	for _, k := range data {
 		var entry = k.(map[string]interface{})
 		var timelog = timelog{}
 
-		timelog.taskID = entry["taskId"].(string)
-		timelog.userID = entry["userId"].(string)
+		var userID = entry["userId"].(string)
+		timelog.user = contacts[userID]
 		timelog.trackedDate = entry["trackedDate"].(string)
 		timelog.hours = entry["hours"].(float64)
 
-		result[i] = timelog
+		var taskID = entry["taskId"].(string)
+		result[taskID] = append(result[taskID], timelog)
 	}
 
 	return result
 }
 
 type task struct {
-	taskID   string
 	fields   map[string]string
 	timelogs []timelog
 }
 
-func getTasks(json map[string]interface{}, customFields map[string]string, timelogs []timelog) []task {
+func getTasks(json map[string]interface{}, customFields map[string]string, timelogs map[string][]timelog) []task {
 	var data = json["data"].([]interface{})
 
-	var result = make([]task, len(data))
-	for i, k := range data {
+	var result = []task{}
+	for _, k := range data {
 		var entry = k.(map[string]interface{})
 		var task = task{}
-
-		task.taskID = entry["id"].(string)
 
 		task.fields = make(map[string]string)
 		var entryFields = entry["customFields"].([]interface{})
@@ -95,7 +96,12 @@ func getTasks(json map[string]interface{}, customFields map[string]string, timel
 			task.fields[fieldName] = field["value"].(string)
 		}
 
-		result[i] = task
+		var taskID = entry["id"].(string)
+		task.timelogs = timelogs[taskID]
+
+		if len(task.fields) > 0 && len(task.timelogs) > 0 {
+			result = append(result, task)
+		}
 	}
 
 	return result
@@ -123,9 +129,9 @@ func main() {
 	var contacts = getContactsMap(json)
 
 	json = getDataForURL(host+"/timelogs", apiKey)
-	var timeLogs = getTimelogs(json)
+	var timeLogs = getTimelogMap(json, contacts)
 
-	json = getDataForURL(host+"/tasks?fields=['customFields']", apiKey)
+	json = getDataForURL(host+"/folders/IEADDL6NI4NMQXDT/tasks?fields=['customFields']", apiKey)
 	var tasks = getTasks(json, customFields, timeLogs)
 
 	fmt.Println(customFields)
